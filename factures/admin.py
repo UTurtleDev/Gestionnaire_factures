efc0d226051775b069b1602bbf54e8335c4725b5
UTurtle <uturtledev@gmail.com>
2025-03-13 17:54:02 +0100

    filtres interface admin

--- factures/admin.py
+++ factures/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Filtres intelligents dans l'interface admin
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'date', 'client', 'amount_ht', 'amount_ttc', 'statut', 'day_late')
+    list_filter = ('date', 'client', 'statut')
+    search_fields = ('invoice_number', 'client__company', 'client__entity_name')
+
+    def day_late(self, obj):
+        if obj.statut == 'en_retard':
+            return (obj.due_date - obj.date).days
+        return 0
+    
+    day_late.short_description = 'Jours en retard'
