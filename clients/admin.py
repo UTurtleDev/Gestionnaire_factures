6b47d23af54c571eddba7137b5d595ef8f5fcd09
UTurtle <uturtledev@gmail.com>
2025-06-11 19:54:25 +0200

    creation class contact

--- clients/admin.py
+++ clients/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Contact
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ from .models import Client
 
 
 admin.site.register(Client)
+admin.site.register(Contact)
--- clients/models.py
+++ clients/models.py
@@ -11,10 +11,68 @@ class Client(models.Model):
     phone_number = models.CharField(max_length=10, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
 
-    def __str__(self):
-        return self.entity_name
     
     
     class Meta:
         verbose_name = "Client"
         verbose_name_plural = "Clients"
+        ordering = ['entity_name']
+
+
+    def __str__(self):
+        return self.entity_name
+    
+    @property
+    def contact_principal(self):
+        return self.contacts.filter(is_principal=True).first()
+    
+    @property
+    def tous_les_contacts(self):
+        return self.contacts.all()
+    
+
+class Contact(models.Model):
+    client = models.ForeignKey('clients.Client', on_delete=models.CASCADE, related_name='contacts')
+    nom = models.CharField(max_length=100, blank=True, null=True)
+    prenom = models.CharField(max_length=100, blank=True, null=True)
+    fonction = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    is_principal = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+        ordering = ['-is_principal', 'nom']
+
+        # Contrainte : un seul contact principal par client
+        constraints = [
+            models.UniqueConstraint(
+                fields=['client', 'is_principal'],
+                condition=models.Q(is_principal=True),
+                name='unique_principal_contact_per_client'
+            )
+        ]
+
+
+    def __str__(self):
+        nom_complet = f"{self.prenom} {self.nom}".strip()
+        if self.fonction:
+            return f"{nom_complet} ({self.fonction})"
+        return nom_complet
+    
+
+    def save(self, *args, **kwargs):
+        # Si on marque ce contact comme principal, 
+        # démarquer les autres contacts principaux du même client
+        if self.is_principal:
+            self.__class__.objects.filter(
+                client=self.client, 
+                is_principal=True
+            ).exclude(pk=self.pk).update(is_principal=False)
+        
+        # Si c'est le premier contact du client, le marquer automatiquement comme principal
+        if not self.__class__.objects.filter(client=self.client).exists():
+            self.is_principal = True
+            
+        super().save(*args, **kwargs)
